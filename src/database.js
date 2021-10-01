bf7b7ebecbf4f21272142dbd92833e0d7821b16d
minterger <minterger@gmail.com>
2021-10-01 19:34:30 -0300

    Update database.js

--- src/database.js
+++ src/database.js
@@ -2,13 +2,11 @@ const mongoose = require("mongoose");
 
 const uri = `mongodb+srv://${process.env.USERNAMEDB}:${process.env.PASSWORD}@${process.env.DBURL}/${process.env.DBNAME}?retryWrites=true&w=majority`;
 
-const main = async () => {
+(async () => {
   try {
     const res = await mongoose.connect(uri);
     console.log("database is connected");
   } catch (err) {
     console.log(err);
   }
-};
-
-main();
+})();
